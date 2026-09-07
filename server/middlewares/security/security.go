cac6ee24e69fb36421b0d10063c3a4c9b9679a3d
Joshua Rich <joshua.rich@gmail.com>
2026-09-07 00:23:19 +0000

    feat(server/middlewares/security): ✨ support customising the Permissions-Policy header

--- server/middlewares/security/security.go
+++ server/middlewares/security/security.go
@@ -5,32 +5,40 @@ package security
 
 import (
 	"net/http"
+	"strings"
 )
 
+var defaultPermissions = []string{"camera=()", "microphone=()", "geolocation=()", "usb=()", "bluetooth=()"}
+
 // GeneralSecurity middleware adds a few response headers to harden against some threats.
-func GeneralSecurity(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
-		// Do not allow embedding.
-		//
-		// https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#x-frame-options
-		res.Header().Set("X-Frame-Options", "DENY")
+func GeneralSecurity(permissions []string) func(next http.Handler) http.Handler {
+	if len(permissions) == 0 {
+		permissions = defaultPermissions
+	}
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+			// Do not allow embedding.
+			//
+			// https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#x-frame-options
+			res.Header().Set("X-Frame-Options", "DENY")
 
-		// Do not allow browsers to guess mime-types.
-		//
-		// https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#x-content-type-options
-		res.Header().Set("X-Content-Type-Options", "nosniff")
+			// Do not allow browsers to guess mime-types.
+			//
+			// https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#x-content-type-options
+			res.Header().Set("X-Content-Type-Options", "nosniff")
 
-		// Enforce referrer origin.
-		//
-		// https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#referrer-policy
-		res.Header().Set("Referrer-Policy", "strict-origin-when-cross-origin")
+			// Enforce referrer origin.
+			//
+			// https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#referrer-policy
+			res.Header().Set("Referrer-Policy", "strict-origin-when-cross-origin")
 
-		// Disable uneeded permissions.
-		res.Header().
-			Set("Permissions-Policy", "camera=(), microphone=(), geolocation=(), usb=(), bluetooth=()")
+			// Disable uneeded permissions.
+			res.Header().
+				Set("Permissions-Policy", strings.Join(permissions, ","))
 
-		next.ServeHTTP(res, req)
-	})
+			next.ServeHTTP(res, req)
+		})
+	}
 }
 
 // CrossOriginProtection middleware adds Cross Origin related security headers.
